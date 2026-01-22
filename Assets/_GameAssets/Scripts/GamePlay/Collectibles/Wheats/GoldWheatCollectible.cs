ce336e61bef6ea93ec43a92d21c16fc134d8644f
Arda Çınar <cinararda71@gmail.com>
2026-01-22 19:17:14 +0300

    v0.11 Arayüz Elemanları ve Sistemleri Eklendi

--- Assets/_GameAssets/Scripts/GamePlay/Collectibles/Wheats/GoldWheatCollectible.cs
+++ Assets/_GameAssets/Scripts/GamePlay/Collectibles/Wheats/GoldWheatCollectible.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GoldWheatCollectible : MonoBehaviour, ICollectible
 {
@@ -6,9 +8,23 @@ public class GoldWheatCollectible : MonoBehaviour, ICollectible
 
     [SerializeField] private PlayerController _playerController;
 
+    [SerializeField] private PlayerStateUI _playerStateUI;
+
+    private RectTransform _playerBoosterTransform;
+    private Image _playerBoosterImage;
+
+    private void Awake()
+    {
+        _playerBoosterTransform = _playerStateUI.GetBoosterSpeedTransform;
+        _playerBoosterImage = _playerBoosterTransform.GetComponent<Image>();
+    }
+
     public void Collect()
     {
         _playerController.SetMovementSpeed(_wheatDesignSO.IncreaseDecreaseMultplier, _wheatDesignSO.ResetBoostDuration);
+
+        _playerStateUI.PlayBoosterUIAnimations(_playerBoosterTransform, _playerBoosterImage, _playerStateUI.GetGoldBoosterWheatImage, _wheatDesignSO.ActiveSprite, _wheatDesignSO.PassiveSprite, _wheatDesignSO.ActiveWheatSprite, _wheatDesignSO.PassiveWheatSprite, _wheatDesignSO.ResetBoostDuration);
+
         Destroy(gameObject);
     }
 }
